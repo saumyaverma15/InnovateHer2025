dad68d1ef722537a564d9bc7754d1d4285f1f41a
trishagodara <trishagodara+programming@gmail.com>
2025-01-26 08:36:45 -0500

    Next button added and formatted!

--- my-app/src/avatarConvo.js
+++ my-app/src/avatarConvo.js
@@ -1,17 +1,28 @@
-import { Grid2, Box } from "@mui/material";
+import { Grid2, Box, Button } from "@mui/material";
 import Sprite from "./avatar";
 
 export default function Conversation({ text }) {
+    var sents = text.split(".");
     return (
         <Grid2
+            container
             display="flex"
             justifyContent="center"
-            alignItems="center"
+            justifySelf={"center"}
             minHeight="100vh"
+            direction="column"
+            sx={{width: 256}}
         >
             <Box>
-                <Sprite text={text} />
+                <Sprite text={sents} />
             </Box>
+            <Grid2 item  display="flex" justifyContent="right">
+                <Button
+                    variant="contained"
+                >
+                    Next
+                </Button>
+            </Grid2>
         </Grid2>
     );
 }
